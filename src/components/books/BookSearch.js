acec3d9a0f547c96ab7037c6eadeb8d9abc2cb5d
Andrew Seedholm <aseedholm@gmail.com>
2020-04-21 16:55:59 -0400

    Commented out API requests to avoid HTTPS issue.

--- src/components/books/BookSearch.js
+++ src/components/books/BookSearch.js
@@ -16,12 +16,12 @@ export default class BookSearch extends React.Component {
         if(searchTitle === undefined) {
             searchTitle = ' '
         }
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTitle}&maxResults=40&orderBy=relevance`)
-            .then(response => response.json())
-            .then(results => this.setState({
-                                               books: results.items,
-                                               bookNameSearch: searchTitle
-                                           }))
+        // fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTitle}&maxResults=40&orderBy=relevance`)
+        //     .then(response => response.json())
+        //     .then(results => this.setState({
+        //                                        books: results.items,
+        //                                        bookNameSearch: searchTitle
+        //                                    }))
         fetch(`http://localhost:8080/api/books/title/${searchTitle}`)
             .then(response => response.json())
             .then(results => this.setState({
@@ -38,12 +38,12 @@ export default class BookSearch extends React.Component {
     }
 
     findBookByName = (title) => {
-        fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=${title}&maxResults=40&orderBy=relevance`)
-            .then(response => response.json())
-            .then(results => this.setState({
-                                               books: results.items
-                                           }));
+        // fetch(
+        //     `https://www.googleapis.com/books/v1/volumes?q=${title}&maxResults=40&orderBy=relevance`)
+        //     .then(response => response.json())
+        //     .then(results => this.setState({
+        //                                        books: results.items
+        //                                    }));
         fetch(`http://localhost:8080/api/books/title/${title}`)
             .then(response => response.json())
             .then(results => this.setState({
